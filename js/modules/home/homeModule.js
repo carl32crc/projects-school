bec128194aff01a44da8727dd610dfbd71414ea5
Carl Ruiz <carl.45.cr@gmail.com>
2016-09-15 23:11:11 +0200

    pages of projects ok

--- js/modules/home/homeModule.js
+++ js/modules/home/homeModule.js
@@ -1,11 +1,13 @@
 angular.module("homeModule", [])
 	.controller("home", function( $rootScope,$scope) {
 
-		$rootScope.section ="home";
+		$rootScope.section ="Home";
 
 		$scope.projects = [
-				{ name: 'Project - 01', url: '#' },
-				{ name: 'Project - 02', url: '#' }];
+				{ name: 'Project - 01', url: '#',
+				description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.'},
+				{ name: 'Project - 02', url: '#',
+				description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit.Laudantium veniam exercitationem expedita laborum at voluptate.Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.' }];
 
 
 });

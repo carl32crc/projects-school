02eba10afd24036860d665ffcf75be841d83bf30
Carl Ruiz <carl.45.cr@gmail.com>
2016-09-17 17:37:07 +0200

    implement articles with angular ok

--- js/modules/home/homeModule.js
+++ js/modules/home/homeModule.js
@@ -3,5 +3,14 @@ angular.module("homeModule", [])
 
 		$rootScope.section ="HOME";
 
+		$scope.articles = [
+				{ name: 'Article - 01', url: '#',
+				description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.'},
+				{ name: 'Article - 02', url: '#',
+				description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit.Laudantium veniam exercitationem expedita laborum at voluptate.Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.'},
+				{ name: 'Article - 03', url: '#',
+				description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.'},
+];
+
 
 });
